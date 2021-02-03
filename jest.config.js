869b8cd48f8d4b7e6445ab8edc55eb591af56600
Eduardo Sotero <eduardoliberatosotero@gmail.com>
2021-02-03 12:57:43 -0300

    ci(dependabot): add valitation for pullrequests

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,11 @@ module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts(x)'],
+  collectCoverageFrom: [
+    'src/**/*.ts(x)?',
+    '!src/**/*.stories.mdx',
+    '!src/**/*.stories.@(js|jsx|ts|tsx)',
+    '!src/**/stories.tsx'
+  ],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
 }
