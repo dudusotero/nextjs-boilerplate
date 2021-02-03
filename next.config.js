e67038ec4011b46ea52be5d1e7ff95e797890734
Eduardo Sotero <eduardoliberatosotero@gmail.com>
2021-02-03 18:18:38 -0300

    chore(pwa): add runtime cache to pwa config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
+/* eslint-disable @typescript-eslint/no-var-requires */
 const withPWA = require('next-pwa')
+const runtimeCaching = require('next-pwa/cache')
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = withPWA({
   pwa: {
     dest: 'public',
-    disable: !isProd
+    disable: !isProd,
+    runtimeCaching
   },
   i18n: {
     locales: ['en-US'],
